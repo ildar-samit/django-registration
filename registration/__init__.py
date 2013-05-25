a3f981006fae846714bdb5aa0de98ac829a57bc0
Ildar Samit <ildar.samit@gmail.com>
2013-05-25 09:27:58 -0400

    Fix dependency order issue in get_version

--- registration/__init__.py
+++ registration/__init__.py
@@ -2,5 +2,8 @@ VERSION = (0, 9, 0, 'beta', 1)
 
 
 def get_version():
-    from django.utils.version import get_version as django_get_version
-    return django_get_version(VERSION) # pragma: no cover
+    try:
+        from django.utils.version import get_version as django_get_version
+        return django_get_version(VERSION) # pragma: no cover
+    except ImportError:
+        return ".".join(str(n) for n in VERSION)
